04ecfb613c58046031b8459ef2a78834afea24ee
User <https://github.com/karno786/Sqlite_git_test>
2019-10-21 19:01:20 +0600

    Recycle View Done.

--- app/src/main/java/com/bitmdeveloper/dailyexpensenote/activity/adapter/ExpenseAdapter.java
+++ app/src/main/java/com/bitmdeveloper/dailyexpensenote/activity/adapter/ExpenseAdapter.java
@@ -2,9 +2,12 @@ package com.bitmdeveloper.dailyexpensenote.activity.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,7 +16,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bitmdeveloper.dailyexpensenote.R;
 import com.bitmdeveloper.dailyexpensenote.activity.model_class.Expense;
+import com.google.android.material.navigation.NavigationView;
 
+import java.sql.Array;
 import java.util.List;
 
 public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHolder> {
@@ -39,6 +44,26 @@ public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHold
         holder.expense_dateTV.setText(expense.getExpense_date());
         holder.expense_amountTV.setText(expense.getExpense_amount());
 
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(context,R.layout.support_simple_spinner_dropdown_item,holder.type);
+        arrayAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+
+        holder.update_delete_SP.setAdapter(arrayAdapter);
+
+        holder.update_delete_SP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selecttype = adapterView.getSelectedItem().toString();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,11 +79,14 @@ public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ViewHold
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView expense_typeTV,expense_dateTV,expense_amountTV;
+        private Spinner update_delete_SP;
+        private String[] type = {"","Update","Delete"};
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             expense_typeTV = itemView.findViewById(R.id.expense_typeTV);
             expense_dateTV = itemView.findViewById(R.id.expense_dateTV);
             expense_amountTV = itemView.findViewById(R.id.expense_amountTV);
+            update_delete_SP = itemView.findViewById(R.id.update_deleteSP);
         }
     }
 }
